60eb3341a99f700ca6b1da3dbdb0d32a9d5e8179
Erik Calmfors <erik.calmfors@tietoevry.com>
2025-07-04 14:18:28 +0200

    Add some design elements

--- src/app/math/page.js
+++ src/app/math/page.js
@@ -209,7 +209,7 @@ export default function MathProblems() {
           <label htmlFor="answer" className={styles.hidden}>Svar:</label>
           <input ref={inputRef} id="answer" type="number" placeholder="?" value={answer} onChange={(e) => setAnswer(e.target.value)} onKeyUp={(e) => e.key === 'Enter' && correctAnswer === null && timer > 0 && checkAnswer()} onClick={() => setStartTimer(true)} onFocus={() => setKeyboardOpen(true)} onBlur={() => setKeyboardOpen(false)} />
           <button className={styles.checkmark} onClick={checkAnswer} disabled={!answer || timer === 0} >
-            <svg width="30" height="30" fill={answer ? '#10a64a' : '#ccc'} id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 101.6"><defs></defs><title>tick-green</title><path d="M4.67,67.27c-14.45-15.53,7.77-38.7,23.81-24C34.13,48.4,42.32,55.9,48,61L93.69,5.3c15.33-15.86,39.53,7.42,24.4,23.36L61.14,96.29a17,17,0,0,1-12.31,5.31h-.2a16.24,16.24,0,0,1-11-4.26c-9.49-8.8-23.09-21.71-32.91-30v0Z" /></svg>
+            <svg fill={answer ? '#10a64a' : '#ccc'} id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 101.6"><defs></defs><title>tick-green</title><path d="M4.67,67.27c-14.45-15.53,7.77-38.7,23.81-24C34.13,48.4,42.32,55.9,48,61L93.69,5.3c15.33-15.86,39.53,7.42,24.4,23.36L61.14,96.29a17,17,0,0,1-12.31,5.31h-.2a16.24,16.24,0,0,1-11-4.26c-9.49-8.8-23.09-21.71-32.91-30v0Z" /></svg>
           </button>
         </div>
           : <p>Laddar...</p>}
